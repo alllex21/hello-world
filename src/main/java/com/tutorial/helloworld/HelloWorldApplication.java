ac70250ad44938a419486860685e9fc7ffedfd72
Aleksandar <aleksandarmiladinovic21@gmail.com>
2021-12-10 19:45:17 +0100

    Initial commit

---
+++ src/main/java/com/tutorial/helloworld/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
